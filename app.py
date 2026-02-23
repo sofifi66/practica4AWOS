04d50677230376a0fab853a412588dc7d6e350b9
sofifi66 <sofiaespinoza057@gmail.com>
2026-02-23 08:49:26 -0800

    Create '/productos' route to retrieve product data
    Added a new route to fetch products from the database.

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request,jsonfy, make_response, session
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/productos')
+def productos():
+    import mysql.connector
+mydb = mysql.connector.connect(
+    host="46.28.42.226",
+    user="u760464709_prueba_usr",
+    password="|Au/mc*H2jH3|",
+    database="u760464709_prueba_bd"
+)
+mycursor= mydb.cursor()
+mycursor.execute("SELECT * FROM productos")
+myresult =mycursor.fetchall()
+return make_response(jsonfy(myresult))
