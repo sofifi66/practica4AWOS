c6b2bbc7dcc9193575003c2b113273bea47326e0
sofifi66 <sofiaespinoza057@gmail.com>
2026-02-23 09:09:49 -0800

    Fix indentation and formatting in app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 @app.route('/productos')
 def productos():
     import mysql.connector
-mydb = mysql.connector.connect(
-    host="46.28.42.226",
-    user="u760464709_prueba_usr",
-    password="|Au/mc*H2jH3|",
-    database="u760464709_prueba_bd"
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_prueba_usr",
+        password="|Au/mc*H2jH3|",
+        database="u760464709_prueba_bd"
 )
-mycursor= mydb.cursor()
+mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM productos")
 myresult =mycursor.fetchall()
 return make_response(jsonfy(myresult))
